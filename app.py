e861c44aa97d434bb99dbe9a5d2cc258befbb9f9
lipenandes <97647724+lipenandes@users.noreply.github.com>
2023-02-17 18:07:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def busca():
         if not results.empty:
             message = ""
             for nome, mesa in results.to_dict('split')['data']:
-                message += f'O convidado "{nome}" está na mesa "{mesa}"<br>'
+                message += f'O convidado "{nome}" está na mesa <button class="table-button" value="{mesa}">{mesa}</button><br>'
         else:
             message = f'O convidado "{search_term}" não foi encontrado'
 
@@ -31,6 +31,13 @@ def busca():
     else:
         return render_template("Busca.html")
 
+@app.route('/mesa/<mesa>')
+def mesa(mesa):
+    results = df.loc[df['Mesa'] == int(mesa), 'Nome']
+    message = f'Os convidados na mesa {mesa} são:<br>'
+    for nome in results:
+        message += f'{nome}<br>'
+    return render_template("Mesa.html", mesa_value=message)
 
 
 
@@ -39,4 +46,3 @@ def busca():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
